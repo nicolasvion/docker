0f5ce1c9ae1fe838b53d44a286ff4a50a87688ac
Nicolas VION <nicolas.vion@icloud.com>
2021-01-26 10:37:37 +0100

    updating workflow

--- doc-as-code/scripts/run_docker.sh
+++ doc-as-code/scripts/run_docker.sh
@@ -4,26 +4,6 @@ if [[ ! -r src/${1} ]]; then
   exit 1
 fi
 
-if [[ $MERMAID_ENABLED == "yes" ]]
-then
-    docker run --rm \
-      -v ${PWD}/src/${1}:/mnt/src/\
-      -v ${PWD}/images/:/mnt/images/\
-      -v ${PWD}/out/:/mnt/out/ \
-      -e DATE=$(git log -1 --date=short --format=%cd) \
-      nicolasvion/mermaid ${1} ${2}
-fi
-
-if [[ $CSV_ENABLED == "yes" ]]
-then
-    docker run --rm \
-      -v ${PWD}/src/${1}:/mnt/src/\
-      -v ${PWD}/images/:/mnt/images/\
-      -v ${PWD}/out/:/mnt/out/ \
-      -e DATE=$(git log -1 --date=short --format=%cd) \
-      nicolasvion/csvtomd ${1} ${2}
-fi
-
 if [[ $BUILD_ENABLED == "yes" ]]
 then
     docker run --rm \
@@ -31,5 +11,5 @@ then
       -v ${PWD}/images/:/mnt/images/\
       -v ${PWD}/out/:/mnt/out/ \
       -e DATE=$(git log -1 --date=short --format=%cd) \
-      nicolasvion/pandoc ${1} ${2}
+      nicolasvion/pandoc:v0.1 ${1} ${2}
 fi
--- doc-as-code/src/notes-2018-02-28/1-header.md
+++ doc-as-code/src/notes-2018-02-28/1-header.md
@@ -1,9 +1,15 @@
 ---
-title: Notes - Resume
+title: Notes
+subtitle: Resume
 author:
-
 - Nicolas VION
-
+date: "2017-02-20"
+subject: "Markdown"
+titlepage: true
+titlepage-text-color: "FFFFFF"
+titlepage-rule-color: "360049"
+titlepage-rule-height: 0
+titlepage-background: "background.pdf"
 subparagraph: yes
 ...
 \newpage
